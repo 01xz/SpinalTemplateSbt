1f149555f0cdd9ee668071adefeb3dd9e35e850e
01xz <73350430+01xz@users.noreply.github.com>
2022-03-30 20:17:23 +0800

    init commit

---
+++ src/main/scala/mylib/MyTopLevel.scala
@@ -0,0 +1,37 @@
+package mylib
+
+import spinal.core._
+import spinal.lib._
+
+import scala.util.Random
+
+class MyTopLevel extends Component {
+  val io = new Bundle {
+    val cond0 = in  Bool()
+    val cond1 = in  Bool()
+    val flag  = out Bool()
+    val state = out UInt(8 bits)
+  }
+  val counter = Reg(UInt(8 bits)) init(0)
+
+  when(io.cond0){
+    counter := counter + 1
+  }
+
+  io.state := counter
+  io.flag  := (counter === 0) | io.cond1
+}
+
+object MyTopLevelVerilog {
+  def main(args: Array[String]) {
+    SpinalVerilog(new MyTopLevel)
+  }
+}
+
+object MySpinalConfig extends SpinalConfig(defaultConfigForClockDomains = ClockDomainConfig(resetKind = SYNC))
+
+object MyTopLevelVerilogWithCustomConfig {
+  def main(args: Array[String]) {
+    MySpinalConfig.generateVerilog(new MyTopLevel)
+  }
+}
